3f425d2d1e680cb75221369e13df810bd9d19401
OLGACAR <67768894+OLGACAR@users.noreply.github.com>
2020-07-20 14:43:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ restService.post("/facturas", function(req, res) {
   
 
 	switch (speech) {
-	case 123:
+	case 1510:
 		speech = 
 			'Fecha: 04/8/2020, IdFact: 3, Valor: $13,000.00 - https://pagosinteligentes.com/ \n Fecha: 10/08/2020, IdFact: 1, Valor: $120,000.00 - https://pagosinteligentes.com/'
 		break;
-	case 564:
+	case 4574:
 		speech = 
 			'Fecha: 08/08/2020, IdFact: 4, Valor: $133,000.00 - https://pagosinteligentes.com/'
 		break;		
