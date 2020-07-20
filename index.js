84728247702dfea70d1dba1d94292dd6fa1a4e90
OLGACAR <67768894+OLGACAR@users.noreply.github.com>
2020-07-20 12:48:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,12 +25,16 @@ restService.post("/facturas", function(req, res) {
 	switch (speech) {
 	case 123:
 		speech = 
-			'Fecha: 4/8/2020, IdFact: 3, Valor: $13,000.00 - https://pagosinteligentes.com/ \n Fecha: 4/20/2020, IdFact: 1, Valor: $120,000.00 - https://pagosinteligentes.com/'
+			'Fecha: 04/8/2020, IdFact: 3, Valor: $13,000.00 - https://pagosinteligentes.com/ \n Fecha: 10/08/2020, IdFact: 1, Valor: $120,000.00 - https://pagosinteligentes.com/'
 		break;
 	case 564:
 		speech = 
-			'Fecha: 4/20/2020, IdFact: 4, Valor: $133,000.00'
+			'Fecha: 08/08/2020, IdFact: 4, Valor: $133,000.00 - https://pagosinteligentes.com/'
 		break;		
+	case 1020:
+		speech = 
+			'Fecha: 25/07/2020, IdFact: 10, Valor: $250,000.00 - https://pagosinteligentes.com/ \n Fecha: 25/08/2020, IdFact: 8, Valor: $100,000.00 - https://pagosinteligentes.com/ \n Fecha: 25/08/2020, IdFact: 20, Valor: $250,000.00 - https://pagosinteligentes.com/'
+		break;				
 	default:
 		speech = 'No tiene facturas pendientes'
 		break;
